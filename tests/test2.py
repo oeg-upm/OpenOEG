cc9ec030e550f808067beec4537b1a09d351935d
Free <jaime.vrivera@gmail.com>
2025-02-15 12:02:15 +0100

    Cambios con config en main bien 2

--- tests/test2.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-with open('config2.yaml', 'r') as yaml_file:
-    config = yaml.safe_load(yaml_file)
-    
-user2 = config["config"]["credentials"]["oeg"]["username"]
-print(user2)
--- tests/textanalysis.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
-
-paginas=open_file('./tfg/paginas.txt')
-
-print(paginas.split("\n")[0])
